325214e848c53f5c62ac8c1edf36e59de8c66678
Daniel Hildreth <pillarite@gmail.com>
2022-03-01 05:59:52 -0500

    Added back to top btn, styled header background

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
-const navToggle = document.querySelector('.mobile-nav-toggle');
-const nav = document.querySelector('.nav');
+// const navToggle = document.querySelector('.mobile-nav-toggle');
+// const nav = document.querySelector('.nav');
 
 
-navToggle.addEventListener('click', () => {
-    nav.classList.toggle('nav--visible');
-})
+// navToggle.addEventListener('click', () => {
+//     nav.classList.toggle('nav--visible');
+// })
+
+//Get the button
+var backBtn = document.getElementById("backBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backBtn.style.display = "block";
+    } else {
+        backBtn.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
